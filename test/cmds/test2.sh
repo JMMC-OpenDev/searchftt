db84e800ec4767d5d47971bee1836536aad9c1d6
mellag <guillaume.mella@univ-grenoble-alpes.fr>
2024-02-19 15:37:35 +0100

    add more inputs to test

---
+++ test/cmds/test2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl  "http://localhost:8080/exist/apps/searchftt/modules/aspro.xql?identifiers=HD+195019"
---
+++ test/cmds/test3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl  "http://localhost:8080/exist/apps/searchftt/modules/aspro.xql?identifiers=HD1;HD2;HD3;HD4;HD5;HD6;HD7;HD8;HD9;HD10;HD11"
